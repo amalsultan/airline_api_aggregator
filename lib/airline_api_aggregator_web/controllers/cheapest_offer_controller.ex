41e2de552e0849af88e9b49fc22b0c843c6c9d2a
amal <amalsultan999@gmail.com>
2021-10-04 01:53:33 +0500

    handles cheapest_offer response in controller

--- lib/airline_api_aggregator_web/controllers/cheapest_offer_controller.ex
+++ lib/airline_api_aggregator_web/controllers/cheapest_offer_controller.ex
@@ -5,8 +5,13 @@ defmodule AirlineApiAggregatorWeb.CheapestOfferController do
 
   action_fallback AirlineApiAggregatorWeb.FallbackController
 
+  @spec show(Plug.Conn.t(), map) :: Plug.Conn.t()
   def show(conn, %{"origin" => origin, "destination" => destination, "departureDate" => departure_date}) do
-    cheapest_offer = CheapestOffer.get_cheapest_offer(%{origin: origin, destination: destination, departure_date: departure_date})
-    render(conn, "show.json", cheapest_offer: cheapest_offer)
+    CheapestOffer.get_cheapest_offer(%{origin: origin, destination: destination, departure_date: departure_date})
+    |> case do
+      {:ok, cheapest_offer} -> render(conn, "show.json", cheapest_offer: cheapest_offer)
+      {:error, message} ->
+        json conn, %{error: %{details: message}}
+    end
   end
 end
