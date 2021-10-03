8e30e7851fc9e8f087df32c625148e4a078a14de
amal <amalsultan999@gmail.com>
2021-10-04 02:10:01 +0500

    adds test case for invalid parameters

--- test/airline_api_aggregator_web/controllers/cheapest_offer_controller_test.exs
+++ test/airline_api_aggregator_web/controllers/cheapest_offer_controller_test.exs
@@ -9,13 +9,20 @@ defmodule AirlineApiAggregatorWeb.CheapestOfferControllerTest do
   end
 
   describe "show" do
-    test "show cheapest offer valid attributes", %{conn: conn} do
+    test "show cheapest offer with valid attributes", %{conn: conn} do
       conn = get(conn, Routes.cheapest_offer_path(conn, :show, @valid_attrs))
       response = json_response(conn, 200)["data"]
       cheapest_offer = Map.get(response, "cheapest_offer")
       assert cheapest_offer != nil
       assert Map.get(cheapest_offer, "airline") != nil
       assert Map.get(cheapest_offer, "amount") != nil
+    end
+  end
+
+  describe "show with invalid parameters" do
+    test "show cheapest offer invalid attributes", %{conn: conn} do
+      conn = get(conn, Routes.cheapest_offer_path(conn, :show, @invalid_attrs))
+      assert json_response(conn, 200)["error"] == %{"details" => "invalid parameters"}
     end
   end
 end
